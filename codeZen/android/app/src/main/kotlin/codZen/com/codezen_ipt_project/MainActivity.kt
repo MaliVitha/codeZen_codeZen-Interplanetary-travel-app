5342c149b65713b9a44e6aef72e9cf4fda12668c
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-08-18 02:11:46 +0530

    Base

---
+++ codeZen/android/app/src/main/kotlin/codZen/com/codezen_ipt_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package codZen.com.codezen_ipt_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ codezen_iptp/android/app/src/main/kotlin/com/example/codezen_iptp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codezen_iptp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
