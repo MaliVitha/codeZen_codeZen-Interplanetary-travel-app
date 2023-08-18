5199c5bf7fbf078edd27b099ea183c12f927f91b
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-08-18 17:39:38 +0530

    image loaded

--- codezen_iptp/lib/main.dart
+++ codezen_iptp/lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:codezen_iptp/splash.dart';
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
-  runApp(MyApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  var FlutterNativeSplash;
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splash(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
--- codezen_iptp/lib/splash.dart
+++ codezen_iptp/lib/splash.dart
@@ -1,54 +1,46 @@
-import 'home.dart';
+// import 'home.dart';
 
-import 'package:codezen_iptp/main.dart';
-import 'package:flutter/material.dart';
+// import 'package:codezen_iptp/main.dart';
+// import 'package:flutter/material.dart';
 
-class Splash extends StatefulWidget {
-  const Splash({super.key});
+// class Splash extends StatefulWidget {
+//   const Splash({super.key});
 
-  @override
-  State<Splash> createState() => _SplashState();
-}
+//   @override
+//   State<Splash> createState() => _SplashState();
+// }
 
-class _SplashState extends State<Splash> {
-  @override
-  void initState() {
-    super.initState();
-    _navigatetohome();
-  }
+// class _SplashState extends State<Splash> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _navigatetohome();
+//   }
 
-  _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 3000), () {});
-    Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MyHomePage(
-                  title: 'GFG',
-                )));
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Container(
-            //   height: 100,
-            //   width: 100,
-            //   color: Colors.blue,
-            // ),
-            Container(
-              // child: Text(
-              //   'Get Started',
-              //   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              // ),
-              child: Image(image: AssetImage('assets/Spash.jpg')),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             // Container(
+//             //   height: 100,
+//             //   width: 100,
+//             //   color: Colors.blue,
+//             // ),
+//             Container(
+//               // child: Text(
+//               //   'Get Started',
+//               //   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//               // ),
+//               child: Image(image: AssetImage('assets/Spash.jpg')),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
