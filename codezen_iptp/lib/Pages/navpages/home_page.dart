82fd902004dced23a0d71b2d1ce83848c13c291b
Malinsha Vithanage <kavindigvm.20@uom.lk>
2023-08-19 23:14:00 +0530

    image uploaded

--- codezen_iptp/lib/Pages/navpages/home_page.dart
+++ codezen_iptp/lib/Pages/navpages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:codezen_iptp/widgets/app_large_text.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: Column(children: [
+        Container(
+          padding: const EdgeInsets.only(top: 70, left: 20),
+          margin: const EdgeInsets.only(right: 20),
+          child: Row(children: [
+            Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Colors.white,
+            ),
+            Expanded(child: Container()),
+            Icon(
+              Icons.notifications,
+              size: 30,
+              color: Colors.white,
+            ),
+            Icon(Icons.account_circle, size: 30, color: Colors.white),
+          ]),
+        ),
+        SizedBox(height: 5),
+        Container(
+            width: 420,
+            height: 223,
+            child: Image(
+              image: AssetImage("img/home_page _img1.png"),
+            ))
+      ]),
+    );
   }
 }
