546af359ce8bfb7e09d921bc2d1a58539ff2ac15
Malinsha Vithanage <kavindigvm.20@uom.lk>
2023-08-19 19:04:15 +0530

    NavBar Done

--- codezen_iptp/lib/Pages/navpages/main_page.dart
+++ codezen_iptp/lib/Pages/navpages/main_page.dart
@@ -32,8 +32,13 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[0],
+      backgroundColor: Colors.black87,
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+          unselectedFontSize: 0,
+          selectedFontSize: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color.fromRGBO(131, 65, 28, 1),
           onTap: onTap,
           currentIndex: currentIndex,
           selectedItemColor: Colors.black54,
