12711a24a48f50fbcbaf0ad32f1ae661c6be0cd6
Malinsha Vithanage <kavindigvm.20@uom.lk>
2023-09-17 12:59:08 +0530

    images added

--- codezen_iptp/lib/Pages/navpages/home_page.dart
+++ codezen_iptp/lib/Pages/navpages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:codezen_iptp/widgets/app_large_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +14,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(children: [
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        //menu text
         Container(
           padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
           child: Row(children: [
@@ -32,6 +34,7 @@ class _HomePageState extends State<HomePage> {
           ]),
         ),
         SizedBox(height: 5),
+        //image
         Container(
             width: 420,
             height: 223,
@@ -39,6 +42,7 @@ class _HomePageState extends State<HomePage> {
               image: AssetImage("img/home_page _img1.png"),
             )),
         SizedBox(height: 5),
+        //middle menu text
         Container(
           padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
           color: Colors.white,
@@ -77,6 +81,7 @@ class _HomePageState extends State<HomePage> {
           child: Row(
               mainAxisAlignment:
                   MainAxisAlignment.center, // Centerize the icons
+
               children: [
                 const Text(
                   "CONTACT",
@@ -111,6 +116,33 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ]),
+        ),
+        SizedBox(height: 7),
+        //best deals text
+        Container(
+          // margin: const EdgeInsets.only(left: 10),
+          width: 500,
+          padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+          color: Colors.white,
+          child: AppLargeText(
+            text: "Best Holiday Deals",
+            size: 20,
+          ),
+        ),
+        ListView.builder(
+          itemCount: 6,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              width: 150,
+              height: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black,
+                  image: DecorationImage(
+                      image: AssetImage("img/hp_img1.png"), fit: BoxFit.cover)),
+            );
+          },
         ),
       ]),
     );
